f0daf3aa08682677bdec9756f3405b4751915cbb
Franklin Ortiz <fortiz@tiresia.co>
2022-08-26 16:13:10 -0500

    ajuste final del paginado y se quitan comentarios del codigo

--- characters.php
+++ characters.php
@@ -2,47 +2,16 @@
 require_once('head.php');
 require_once('nav.php');
 
-/*
-if(isset($_GET['data'])){
-    echo "lego GET data".$_GET['data'];
-}else{
-    echo "no llego GET data";
-}
-echo "<hr>";
-if(isset($_GET["page"])){
-    echo "lego GET page ".$_GET['page'];
-}else{
-    echo "no llego GET page";
-}*/
 
 $characters = json_decode(file_get_contents($_GET['data']), true);
+$cadena = $_GET['data'];
+$pag_actual;
 
-
-
-//$characters = json_decode(file_get_contents('https://rickandmortyapi.com/api/character?page=3'), true);
-/*
-foreach($characters as $key2 => $value){
-  
-    echo "<hr>";
-    echo "$key";
-   if($key2 == 'info' ){
-    echo 'hola info';
-    foreach($value as $keyinfo => $interno){
-        echo "<br>";
-        echo "entro a info value =>".$keyinfo;
-        echo  ' <a class="btn btn-primary"  role="button">  '.$keyinfo.' </a>';
+    if($cadena[strlen($cadena)-1] == 'r'){
+        $pag_actual = 1;
+    }else{
+        $pag_actual = $cadena[strlen($cadena)-1];
     }
-   }
-    echo "paso";
-  // if($key == 'results' ){
-  //  echo 'hola results';
- //  }
-  
-  // echo  ' <a class="btn btn-primary" href="'.$key.'.php" role="button" value="'.$value.'">  '.$key.' </a>';
-   
-}*/
-
-//echo "<hr>"
 ?>
     <div class="modal" id="modal">
         <div class="modal-background"></div>
@@ -81,34 +50,20 @@ if($key2 == 'info' ){
                         $prev = $interno;
                      break;
                 }
-          if($keyinfo == 'pages') {
-            
-            ?>
-            <h1>PAGES = <?php
-                        echo $pages;
-                        ?> de 
-                
-                        <?php echo $interno ; ?>
-            </h1>
-        <?php } ?>
-       
-        <?php 
     }
     ?>
-    <div class=" container row justify-content-center text-center">
+    <div class=" container row justify-content-center text-center align-items-center">
             <?php  if($prev) {?>
-                <div class="col-sm-1"><a class="fs-3 text " aria-current="page"  href="characters.php?data=<?php echo $prev ?>"><ion-icon name="arrow-back-outline"></ion-icon></a></div>
+                <div class="col-sm-1"><a class="fs-4 text " aria-current="page"  href="characters.php?data=<?php echo $prev ?>"><ion-icon name="arrow-back-outline"></ion-icon></a></div>
             <?php }else{?>
-                <div class="col-sm-1"><a class="fs-3 text " aria-current="page"  href="#"><ion-icon name="arrow-back-outline"></ion-icon></a></div>
+                <div class="col-sm-1"><a class="fs-4 text " aria-current="page"  href="#"><ion-icon name="arrow-back-outline"></ion-icon></a></div>
             <?php } ?>
-            <?php
+            <h1 class="col-sm-1"><?php echo $pag_actual; ?></h1>
             
-            
-            ?>
             <?php  if($next) {?>
-                <div class="col-sm-1"><a class="fs-3 text " aria-current="page"  href="characters.php?data=<?php echo $next ?>"><ion-icon name="arrow-forward-outline"></ion-icon></a></div>
+                <div class="col-sm-1"><a class="fs-4 text " aria-current="page"  href="characters.php?data=<?php echo $next ?>"><ion-icon name="arrow-forward-outline"></ion-icon></a></div>
             <?php }else{?>
-                <div class="col-sm-1"><a class="fs-3 text " aria-current="page"  href="characters.php?data=<?php echo $next ?>"><ion-icon name="arrow-forward-outline"></ion-icon></a></div>
+                <div class="col-sm-1"><a class="fs-4 text " aria-current="page"  href="characters.php?data=<?php echo $next ?>"><ion-icon name="arrow-forward-outline"></ion-icon></a></div>
             <?php } ?>
             
     </div>
@@ -143,22 +98,17 @@ foreach($characters as $key2 => $results){
             }
             ?>
             <div class="col-3">
-            <div class="card" style="width: 18rem;">
-                <a class="card-img-top" onclick="showCharacter('<?php echo $url ?>')"><img src="<?php echo $ima ?>" id="imagen" alt="<?php echo $nombre ?>"></a>
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo $nombre ?></h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="card" style="width: 18rem;">
+                    <a class="card-img-top" onclick="showCharacter('<?php echo $url ?>')"><img src="<?php echo $ima ?>" id="imagen" alt="<?php echo $nombre ?>"></a>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $nombre ?></h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
-            </div>
-            
-                
             </div>
                 
             <?php
-            
-        // echo json_encode($interno);
-        //echo $key2;
         }
     }
 } 
