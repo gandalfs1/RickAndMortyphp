d3a39492ef87b47cd0fc6a476649fba8ee18b330
gandalfs <franklinortiz93@hotmail.com>
2022-08-28 22:18:58 -0500

    ajuste pir de pagina

--- pie.php
+++ pie.php
@@ -1,5 +1,8 @@
-<footer class="footer">
+
+<br />
+<footer class=" footer bg-dark">
     <div class="content has-text-centered">
-        <h1>Desarrollado y diseñador por <span>Franklin Ortiz</span></h1>
+        <h1 class="text-light">Desarrollado y diseñador por <span>Franklin Ortiz</span></h1>
+        <h2 class="text-light"><strong><a href="https://rickandmortyapi.com/" target="_blank">API RICK & MORTY</a></strong></h2>
     </div>
 </footer>
